c9325a1b4c3caeb9d0060a33ec8bac2550a492ed
Jason Thompson <jason@jthompson.ca>
2013-11-11 16:17:24 -0500

    Add toc method to UnderFire module

--- lib/under_fire.rb
+++ lib/under_fire.rb
@@ -5,4 +5,9 @@ require 'under_fire/registration'
 require 'under_fire/api_response'
 
 module UnderFire
+  def disc_toc
+    `discid`.split[2..-2].join " "
+  end
+
+  module_function :disc_toc
 end
