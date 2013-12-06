f1b3bd389d794a39d04752d70988b912562d9558
Jason Thompson <jason@jthompson.ca>
2013-12-06 14:50:10 -0500

    Start to add documentation to AlbumSearch

--- lib/under_fire/album_search.rb
+++ lib/under_fire/album_search.rb
@@ -2,9 +2,14 @@ require 'under_fire/base_query'
 require 'builder'
 
 module UnderFire
+  # Builds XML for Gracenote's ALBUM_SEARCH query
+  # @see https://developer.gracenote.com/sites/default/files/web/html/index.html#Music%20Web%20API/ALBUM_SEARCH.html#_Toc344907249
+  #
   class AlbumSearch < BaseQuery
-    attr_accessor :artist, :track_title, :album_title, :query, :parameters
+    attr_reader :artist, :track_title, :album_title, :query, :parameters
 
+    # @param args [{Symbol => String}]
+    #
     def initialize(args={})
       super args[:mode]
       @parameters = args.reject {|k,v| k == :mode}
