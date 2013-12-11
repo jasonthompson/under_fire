f9e3a6f20e39ba181d4e49c34ae8a5c010a6ce87
Jason Thompson <jason@jthompson.ca>
2013-12-11 05:54:55 -0500

    Add uf alias to under_fire executable

--- lib/under_fire/version.rb
+++ lib/under_fire/version.rb
@@ -1,3 +1,10 @@
 module UnderFire
-  VERSION = "0.0.1"
+  # Because I always forget:
+  #
+  # Given a version number MAJOR.MINOR.PATCH, increment the:
+  #   MAJOR version when you make incompatible API changes,
+  #   MINOR version when you add functionality in a backwards-compatible manner, and
+  #   PATCH version when you make backwards-compatible bug fixes.
+  #   Refer to: http://semver.org/
+  VERSION = "0.6.0"
 end
