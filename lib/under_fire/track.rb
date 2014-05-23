7f1a395242a3941d3206e5bacf4b8cb89fb154a3
Jason Thompson <jason@jthompson.ca>
2014-05-23 14:48:36 -0400

    Add track specs

--- lib/under_fire/track.rb
+++ lib/under_fire/track.rb
@@ -6,9 +6,13 @@ module UnderFire
     # @return [String] Gracenote ID
     attr_reader :gn_id
 
+    # @return [Integer] Track number on album
+    attr_reader :track_number
+
     def initialize(track_info)
       @title = track_info[:title]
       @gn_id = track_info[:gn_id]
+      @track_number = track_info[:track_num].to_i
     end
   end
 end
--- spec/lib/under_fire/track_spec.rb
+++ spec/lib/under_fire/track_spec.rb
@@ -1,12 +1,29 @@
 require_relative '../../spec_helper'
-require 'under_fire/song.rb'
+require 'under_fire/track'
 
 module UnderFire
   describe Track do
-    track = {:track_num => "5",
-             :gn_id => "3843674-5020FA6C0AD103AF757FC8F4DE56F179",
-             :title => "Money"}
-    subject{Track.new(track_info)} 
+    track_info = {:track_num => "5",
+                  :gn_id => "3843674-5020FA6C0AD103AF757FC8F4DE56F179",
+                  :title => "Money"}
+    subject{Track.new(track_info)}
 
+    describe 'title' do
+      it "returns the title" do
+        subject.title.must_equal "Money"
+      end
+    end
+
+    describe 'gn_id' do
+      it "must return the gracenote ID" do
+        subject.gn_id.must_equal "3843674-5020FA6C0AD103AF757FC8F4DE56F179"
+      end
+    end
+
+    describe 'track_number' do
+      it 'must return the track number' do
+        subject.track_number.must_equal 5
+      end
+    end
   end
 end
